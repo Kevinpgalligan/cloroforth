5eae418f6a47a0e9df494f27ae38b32333939a18
Kevin Galligan <galligankevinp@gmail.com>
2026-04-21 21:15:29 +0100

    Tests & dictionary-wrangling.

--- src/interpret.lisp
+++ src/interpret.lisp
@@ -9,6 +9,12 @@
    :fpop
    :fpush-ret
    :fpop-ret
+   :read-dictionary-entry
+   :write-dictionary-entry!
+   :make-dictionary-entry
+   :name
+   :codeword-id
+   :immediate?
    ))
 
 (in-package cloroforth.interpret)
@@ -22,24 +28,26 @@
 (defparameter *return-stack* nil "Return stack, an actual CL data structure.")
 (defparameter *mem* nil "The memory array.")
 (defparameter *cell-size* nil "The number of bytes used by a single cell (a.k.a. 'machine word').")
+(defparameter *int-upper-bound* nil "Upper bound on integers, given a cell size & the use of two's complement.")
 
 (defun make-byte-array (size)
   (make-array size :element-type '(unsigned-byte 8)))
 
 ;;; Initialising the state of the interpreter.
 ;; Default system memory is currently ~2MB, should be enough to store the dictionary & parameter stack.
+;; And 1MB for the user.
 (defun init-forth (&key (cell-size 4) (max-line-size 1000)
                      (system-memory 2097152) (user-memory 1048576))
   (let ((total-memory (+ system-memory max-line-size user-memory)))
     (assert (> (expt 2 (* cell-size 8)) total-memory))
     (setf *cell-size* cell-size)
+    (setf *int-upper-bound* (expt 2 (1- (* 8 *cell-size*))))
     (setf *sp* system-memory)
     (setf *base* system-memory)
-    (setf *rp* nil)
+    (setf *return-stack* nil)
     (setf *mem* (make-byte-array total-memory))))
 
 ;;; Memory utilities.
-;; TODO Need to handle negative numbers, i.e. twos complement.
 (defun read-cell (mem address cell-size)
   (loop with value = 0
         for i from address below (+ address cell-size)
@@ -47,11 +55,9 @@
         do (setf value (+ (aref mem i) (* value 256)))
         finally (return value)))
 
-(defun write-cell! (mem address value cell-size)
+(defun write-cell! (mem address cell-size value)
   (loop for i from (+ address cell-size -1) downto address
         do (progn
-             ;; TODO I think there's a way to get the quotient and remainder with
-             ;; one function call & multiple-value-bind.
              (setf (aref mem i) (mod value 256))
              (setf value (ash value -8)))))
 
@@ -60,7 +66,13 @@
   (read-cell *mem* address *cell-size*))
 
 (defun fwrite-cell! (address value)
-  (write-cell! *mem* address value *cell-size*))
+  (write-cell! *mem* address *cell-size* value))
+
+(defun fread-byte (address)
+  (aref *mem* address))
+
+(defun fwrite-byte (address value)
+  (setf (aref *mem* address) value))
 
 ;; Parameter stack, in user-accessible memory.
 (defun fpush (value)
@@ -73,13 +85,14 @@
     (error "Tried to pop from empty parameter stack!"))
   (let ((v (read-cell *mem* *sp* *cell-size*)))
     (fread-cell *sp*)
-    (incf *sp* *cell-size*)))
+    (incf *sp* *cell-size*)
+    v))
 
 ;; Return stack, not directly accessible to the user.
 (defun fpush-ret (value)
   (push value *return-stack*))
 
-(defun fpop-ret (value)
+(defun fpop-ret ()
   (when (null *return-stack*)
     (error "Tried to pop from empty return stack!"))
   (pop *return-stack*))
@@ -88,6 +101,53 @@
   "Moves the instruction pointer forward."
   (incf *ip* *cell-size*))
 
+;;; The dictionary.
+;; 1 byte for flags (currently just immediate mode flag) + length, name, address of
+;; next entry, codeword id (1 byte), and a blob of data.
+(defclass dictionary-entry ()
+  ((name :initarg :name :reader name)
+   (codeword-id :initarg :codeword-id :reader codeword-id)
+   (immediate? :initarg :immediate? :reader immediate?)))
+
+(defun make-dictionary-entry (name codeword-id immediate?)
+  (make-instance 'dictionary-entry
+                 :name name
+                 :codeword-id codeword-id
+                 :immediate? immediate?))
+
+(defun write-dictionary-entry! (mem address cell-size prev-entry-address entry)
+  (setf (aref mem address)
+        (logxor (if (immediate? entry) (ash 1 7) 0)
+                (length (name entry))))
+  (incf address)
+  ;; Max length of word names will depend on the number of flags squashed into
+  ;; the length byte, currently it's just 1.
+  (when (>= (length (name entry)) (ash 1 7))
+    (error "Word name is too long."))
+  (loop for c across (name entry)
+        do (progn
+             (setf (aref mem address) (char-code c))
+             (incf address)))
+  (write-cell! mem address cell-size prev-entry-address)
+  (incf address cell-size)
+  (setf (aref mem address) (codeword-id entry))
+  (incf address)
+  address)
+
+(defun read-dictionary-entry (mem address cell-size)
+  (let* ((b (aref mem address))
+         (name-length (logandc2 b (ash 1 7)))
+         (immediate? (not (= b name-length)))
+         (name (coerce (loop for i from (1+ address) below (+ address 1 name-length)
+                             collect (code-char (aref mem i)))
+                       'string)))
+    (incf address (+ 1 name-length)) ; skip length byte + name
+    (let ((link (read-cell mem address cell-size)))
+      (incf address cell-size)
+      (let ((codeword-id (aref mem address)))
+        (values
+         (make-instance 'dictionary-entry :immediate? immediate? :name name :codeword-id codeword-id)
+         link)))))
 
 (when nil
 
--- t/memory.lisp
+++ t/memory.lisp
@@ -6,10 +6,52 @@
 (in-suite memory)
 
 (test read-and-write-cell
-      (let ((xs (make-byte-array 10))
-            (v 285)
-            (addr 0)
-            (cell-size 4))
-        (write-cell! xs addr v cell-size)
-        (format t "~%~a~%" xs)
-        (is (= v (read-cell xs addr cell-size)))))
+  (let ((xs (make-byte-array 10))
+        (v 285)
+        (addr 0)
+        (cell-size 4))
+    (write-cell! xs addr cell-size v)
+    (is (= v (read-cell xs addr cell-size)))))
+
+(test push-and-pop
+  (init-forth)
+  (let ((v1 12345)
+        (v2 54321))
+    (fpush v1)
+    (fpush v2)
+    (is (= v2 (fpop)))
+    (is (= v1 (fpop)))))
+
+(test pop-when-empty-stack
+  (init-forth)
+  (fiveam:signals error
+    (fpop)))
+
+(test push-pop-return-stack
+  (init-forth)
+  (let ((v1 123456789)
+        (v2 888888888))
+    (fpush-ret v1)
+    (fpush-ret v2)
+    (is (= v2 (fpop-ret)))
+    (is (= v1 (fpop-ret)))))
+
+(test pop-ret-when-empty
+  (init-forth)
+  (fiveam:signals error
+    (fpop-ret)))
+
+(test dictionary-serialisation
+  (let ((mem (make-byte-array 10000))
+        (d1 (make-dictionary-entry "hello" 0 nil))
+        (d2 (make-dictionary-entry "goodbye" 1 t)))
+    (let ((d2-addr (write-dictionary-entry! mem 0 2 0 d1)))
+      (write-dictionary-entry! mem d2-addr 2 0 d2)
+      (let ((re-d1 (read-dictionary-entry mem 0 2))
+            (re-d2 (read-dictionary-entry mem d2-addr 2)))
+        (is (string= (name d1) (name re-d1)))
+        (is (= (codeword-id d1) (codeword-id re-d1)))
+        (is (eq (immediate? d1) (immediate? re-d1)))
+        (is (string= (name d2) (name re-d2)))
+        (is (= (codeword-id d2) (codeword-id re-d2)))
+        (is (eq (immediate? d2) (immediate? re-d2)))))))
